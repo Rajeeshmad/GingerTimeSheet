b343dcaa46464cabed72287c3cd78348b1845e68
Meghana <meghanavrajan@live.com>
2017-10-02 22:06:28 +0530

    added entity framework and bootstrap

---
+++ GingerTimeSheet/GingerTimeSheet/Models/Account/AccountDetail.cs
@@ -0,0 +1,26 @@
+﻿using GingerTimeSheet.Models.Base;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GingerTimeSheet.Models
+{
+    public class AccountDetail: ModelBase
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        
+        public Client Client{ get; set; }
+
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+        
+        public AccountDetail Parent { get; set; }
+
+        
+        public Employee Assignee { get; set; }        
+    }
+}
---
+++ GingerTimeSheet/GingerTimeSheet/Models/Interface/IModelTimeStamp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GingerTimeSheet.Models.Interface
+{
+    interface IModelTimeStamp
+    {
+        string RowVersion { get; set; }
+    }
+}
